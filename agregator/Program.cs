b090d23dc3b0e89233c2b81b72c301acc31d7689
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2024-01-18 18:23:59 +0100

    cm3

--- agregator/Program.cs
+++ agregator/Program.cs
@@ -1,22 +1,11 @@
-﻿#nullable disable
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
 internal class Program
 {
 
-    const string test = @"7
-192.168.0.11 peter 33
-10.10.17.33 alex 12
-10.10.17.35 peter 30
-10.10.17.34 peter 120
-10.10.17.34 peter 120
-212.50.118.81 alex 46
-212.50.118.81 alex 4";
-
-
+  
     static void Main(string[] args)
     {
         Console.SetIn(new System.IO.StringReader(test)); // do usunięcia przy ocenie
