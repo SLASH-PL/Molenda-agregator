991d55935b0f47f01ac4d4737c465d2997d3a7f3
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2024-01-18 17:37:35 +0100

    cm1

--- agregator/Program.cs
+++ agregator/Program.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿#nullable disable
+
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 internal class Program
 {
+
+    const string test = @"7
+192.168.0.11 peter 33
+10.10.17.33 alex 12
+10.10.17.35 peter 30
+10.10.17.34 peter 120
+10.10.17.34 peter 120
+212.50.118.81 alex 46
+212.50.118.81 alex 4";
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.SetIn(new System.IO.StringReader(test)); // do usunięcia przy ocenie
+
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 0; i < n; i++) 
+        {
+            var linia = Console.ReadLine();
+            var dane = linia.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string ip = dane[0];
+            string login = dane[1];
+            int czas = int.Parse(dane[2]);
+
+            //Console.WriteLine($"{ip} / {login} / {czas}");
+            
+            //umieszczanie danych w strukturach (pamięci)
+
+        }
     }
 }
