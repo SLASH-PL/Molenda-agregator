3d4101aa868b6e94820c57e13473f4bbf45a96d5
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2024-01-18 18:04:37 +0100

    cm2

--- agregator/Program.cs
+++ agregator/Program.cs
@@ -21,6 +21,9 @@ internal class Program
     {
         Console.SetIn(new System.IO.StringReader(test)); // do usunięcia przy ocenie
 
+
+        var login_czas = new SortedDictionary<string, int>();
+        var login_ip = new SortedDictionary<string, SortedSet<string>>();
         int n = int.Parse(Console.ReadLine());
         for(int i = 0; i < n; i++) 
         {
@@ -33,7 +36,24 @@ internal class Program
             //Console.WriteLine($"{ip} / {login} / {czas}");
             
             //umieszczanie danych w strukturach (pamięci)
+            if (login_czas.ContainsKey(login))
+            {   //update
+                login_czas[login] += czas;
+                login_ip[login].Add(ip);
+            }
+            else
+            {   //insert
+                login_czas.Add(login, czas);
+                login_ip.Add(login, new SortedSet<string>());
+                login_ip[login].Add(ip);
+            }
 
+            
+        }
+
+        foreach(var login in login_czas.Keys) 
+        {
+            Console.WriteLine($"{login}: {login_czas[login]} [{string.Join(", ", login_ip[login])}]");
         }
     }
 }
